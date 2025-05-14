41e99b1e1a8ea990ee317554d5e8c571b97bbf47
ArielZhaoLiu <zliu03@mynbcc.com>
2025-05-14 10:35:07 -0300

    Add a filter static

--- src/components/SearchHeader.vue
+++ src/components/SearchHeader.vue
@@ -6,13 +6,28 @@
             v-model="searchTerm" placeholder="Search for recipes..." class="searchFood" />
         <button @click="search" class="search">Search</button>
     </header>
+    <div class="filter">
+        <label for="cuisine">Filter by Cuisines: </label>
+        <select name="" id="cuisine" v-model="selectedCuisine"  @change="handleCuisineChange">
+            <option value="">All</option>
+            <option v-for="cuisine in cuisines" :key="cuisine" :value="cuisine">{{ cuisine }}</option>
+        </select>
+    </div>
 </template>
 
 <script>
 export default {
     data() {
         return {
-            searchTerm: ''
+            searchTerm: '',
+            cuisines: [
+                'African', 'Asian', 'American', 'British', 'Cajun', 'Caribbean',
+                'Chinese', 'Eastern European', 'European', 'French', 'German',
+                'Greek', 'Indian', 'Irish', 'Italian', 'Japanese', 'Jewish',
+                'Korean', 'Latin American', 'Mediterranean', 'Mexican',
+                'Middle Eastern', 'Nordic', 'Southern', 'Spanish', 'Thai', 'Vietnamese'
+            ],
+            selectedCuisine: ''
         }
     },
     methods: {
@@ -22,11 +37,38 @@ export default {
                 return
             }
             this.$emit('search', this.searchTerm)
+        },
+        handleCuisineChange() {
+            this.$emit('cuisine-change', this.selectedCuisine);
         }
     }
 }
 </script>
 
 <style>
+.filter {
+  margin: 1.25rem 0.8rem;
+  font-family: 'Helvetica Neue', sans-serif;
+}   
+
+.filter label {
+  font-size: 1.125rem;
+  margin-right: 0.5rem;
+  color: #a44b4b;
+}
+
+.filter select {
+  padding: 0.3rem 0.3rem;
+  font-size: 1rem;
+  border: 1px solid #c17c7c;
+  border-radius: 6px;
+  background-color: #fff;
+  color: #5a2d2d;
+  transition: border-color 0.2s ease;
+}
+
+.filter select:focus {
+  border-color: #a44b4b;
+}
 
 </style>
